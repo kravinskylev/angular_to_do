86fc6c0c1fc570f8328453823008720fb5d0dce1
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-27 15:19:04 -0400

    second controller

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
 var module = angular.module('todoApp', []);
 
 module.controller('mainController', function($scope) {
+  $scope.formData = {foo: "bar"};
+});
 
+module.controller('secondController' function($scope) {
+  $scope.formData = {hey: "yaaa"}
 });
