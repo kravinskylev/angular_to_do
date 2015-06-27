0198e55cc2d841c2855539fdba45cf78f3684d5e
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-27 15:32:56 -0400

    factories and services

--- public/main.js
+++ public/main.js
@@ -1,9 +1,28 @@
 var module = angular.module('todoApp', []);
 
-module.controller('mainController', function($scope) {
+module.controller('mainController', function($scope, mySampleFactory, mySampleService) {
   $scope.formData = {foo: "bar"};
+
+  $scope.serviceData = mySampleService.serviceData;
 });
 
-module.controller('secondController' function($scope) {
-  $scope.formData = {hey: "yaaa"}
+module.controller('secondController' function($scope, mySampleFactory) {
+  $scope.formData = {hey: "yaaa"};
+
+  $scope.factoryData = mySampleFactory.factoryData;
+});
+
+module.factory('mySampleFactory', function() {
+  var factoryObj = {};
+
+  factoryObj.factoryData = [1,2,3,4,5];
+  factoryObj.alertMessage = function() {
+   alert('HEY');
+  };
+
+  return factoryObj;
+});
+
+module.service('mySampleService', function() {
+  this.serviceData = [6,7,8,9];
 });
