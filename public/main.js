93dd5af733bc3d6466a3ef90d88b4a4846b0091f
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-27 15:39:09 -0400

    cleaning up

--- public/main.js
+++ public/main.js
@@ -1,28 +1,20 @@
 var module = angular.module('todoApp', []);
 
-module.controller('mainController', function($scope, mySampleFactory, mySampleService) {
+module.controller('mainController', function($scope) {
   $scope.formData = {foo: "bar"};
-
-  $scope.serviceData = mySampleService.serviceData;
 });
 
-module.controller('secondController' function($scope, mySampleFactory) {
-  $scope.formData = {hey: "yaaa"};
-
-  $scope.factoryData = mySampleFactory.factoryData;
-});
-
-module.factory('mySampleFactory', function() {
-  var factoryObj = {};
-
-  factoryObj.factoryData = [1,2,3,4,5];
-  factoryObj.alertMessage = function() {
-   alert('HEY');
-  };
-
-  return factoryObj;
-});
-
-module.service('mySampleService', function() {
-  this.serviceData = [6,7,8,9];
-});
+//module.factory('mySampleFactory', function() {
+//  var factoryObj = {};
+//
+//  factoryObj.factoryData = [1,2,3,4,5];
+//  factoryObj.alertMessage = function() {
+//   alert('HEY');
+//  };
+//
+//  return factoryObj;
+//});
+//
+//module.service('mySampleService', function() {
+//  this.serviceData = [6,7,8,9];
+//});
